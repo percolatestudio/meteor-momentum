65d1a4e48c54d24df41894db37e93636b07af7e7
Tom Coleman <tom@thesnail.org>
2014-08-01 20:40:09 +1000

    Made CSS plugin much more useful

--- plugins/css.js
+++ plugins/css.js
@@ -1,18 +1,31 @@
+// defaults, should be overrideable
 var OFFSCREEN_CLASS = 'off-screen';
+var IN_CLASS = 'in';
+var OUT_CLASS = 'out';
+
 var EVENTS = 'webkitTransitionEnd oTransitionEnd transitionEnd ' 
   + 'msTransitionEnd transitionend';
 
 Momentum.registerPlugin('css', function() {
   return {
     insertElement: function(node, next) {
+      console.log('insertElement', node)
       $(node)
         .addClass(OFFSCREEN_CLASS)
+        .addClass(IN_CLASS)
         .insertBefore(next);
   
       Deps.afterFlush(function() {
-        // call width to force the browser to draw it
-        $(node).width();
-        $(node).removeClass(OFFSCREEN_CLASS);
+        // call width to force the browser to draw before we do anything
+        $(node).width()
+        
+        // now bring it in
+        $(node)
+          .removeClass(OFFSCREEN_CLASS)
+          .on(EVENTS, function(e) {
+            if (e.target === node)
+              $(node).removeClass(IN_CLASS);
+          });
       });
     },
       // we could do better I guess?
@@ -21,9 +34,18 @@ Momentum.registerPlugin('css', function() {
       this.insertElement(node, next);
     },
     removeElement: function(node) {
-      $(node).addClass(OFFSCREEN_CLASS)
-        .on(EVENTS, function() {
-          $(node).remove()
+      console.log('removeElement', node)
+      // add the out class and redraw
+      $(node)
+        .addClass(OUT_CLASS)
+        .width();
+      
+      // now make it transition off
+      $(node)
+        .addClass(OFFSCREEN_CLASS)
+        .on(EVENTS, function(e) {
+          if (e.target === node)
+            $(node).remove()
         });
     }
   }

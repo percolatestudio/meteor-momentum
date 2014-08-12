315818d166e1bb64aae58685e9c7cbbac67b7b66
Tom Coleman <tom@thesnail.org>
2014-08-12 16:32:02 +1000

    POC for force re-render.

--- momentum.js
+++ momentum.js
@@ -18,6 +18,21 @@ Template.momentum.rendered = function() {
   this.lastNode._uihooks = hooks;
 }
 
+UI.registerHelper('_momentumTrigger', Template.__create__('_momentumTrigger', function() {
+  var momentumView = Blaze.getParentView(this, 'Template.momentum');
+  
+  console.log(momentumView.parentView, momentumView.parentView.___isTemplateWith, momentumView.parentView.dataVar.get()['force'])
+  
+  // XXX: there should be an easier way of doing this.
+  //   we could also use DynamicTemplate.getInclusionArguments(momentumView)
+  if (momentumView.parentView && momentumView.parentView.___isTemplateWith)
+    // setup a reactive depedency on the "force" option
+    momentumView.parentView.dataVar.get()['force'];
+  
+  return this.templateContentBlock;
+}));
+
+
 Momentum = {
   plugins: {},
   registerPlugin: function(name, plugin) {

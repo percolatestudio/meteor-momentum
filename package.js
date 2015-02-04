7c59d9aa9bead2c1efeee786530c655fc9861568
Zoltan Olah <zol@percolatestudio.com>
2015-02-04 13:42:11 -0800

    Version 0.7.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Reactive animations",
-  version: "0.7.1",
+  version: "0.7.2",
   name: "percolate:momentum",
   git: "https://github.com/percolatestudio/meteor-momentum.git"
 });
